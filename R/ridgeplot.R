4512f8d70e96732d7891b42755eaa3704cd77f5c
ArtPoon <artpoon@gmail.com>
2019-07-29 00:09:21 -0400

    working on ridge plot

--- R/ridgeplot.R
+++ R/ridgeplot.R
@@ -1,4 +1,5 @@
-ridgeplot <- function(x, ..., labels, add.grid=F) {
+ridgeplot <- function(x, ..., labels, add.grid=F, step=0.1,
+                      xlab=NA, ylab='Density', col=NA, bg=NA) {
   # Args:
   #   x: either the first vector followed by additional vectors, or a list
   #      produced by calling split().
@@ -6,7 +7,7 @@ ridgeplot <- function(x, ..., labels, add.grid=F) {
   
   if (is.list(x)) {
     # check that list contains numeric vectors
-    if (!all(sapply(x, class))) {
+    if (!all(sapply(x, is.numeric))) {
       stop("List x must contain only numeric vectors")
     }
   } else if (is.numeric(x)) {
@@ -20,11 +21,23 @@ ridgeplot <- function(x, ..., labels, add.grid=F) {
     stop("Unsupported class of argument x (must be list or numeric)")
   }
   
-  # generate plot region
-  plot(NA, xlim=range(unlist(x)), ylim=c(1, length(x)))
-  
   # generate kernel densities
-  kd <- lapply(x, density)
+  kdens <- lapply(x, density)
+  all.x <- c(sapply(kdens, function(k) k$x))
+  all.y <- c(sapply(1:length(kdens), function(i) {
+    kdens[[i]]$y + i*step
+  }))
+  
+  # generate plot region
+  plot(NA, xlim=range(all.x), ylim=range(all.y), 
+       xlab=xlab, ylab=ylab)
+  
+  for (i in 1:length(kdens)) {
+    kd <- kdens[[i]]
+    y <- kd$y+i*step
+    lines(kd$x, y)
+    abline(h=min(y), col='grey')
+  }
 }
 
 
